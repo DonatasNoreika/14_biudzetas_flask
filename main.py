51efdb0838338fec8435d7918515375642ce77d2
Donatas Noreika <donoras@gmail.com>
2020-10-24 18:26:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+if __name__ == "__main__":
+    from models.irasas import Irasas
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'biudzetas.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+db.create_all()
+
+@app.route("/prideti", methods=['GET', 'POST'])
+def prideti():
+    if request.method == "POST":
+        suma = request.form['suma']
+        info = request.form['info']
+        irasas = Irasas(suma, info)
+        db.session.add(irasas)
+        db.session.commit()
+        return sarasas()
+    elif request.method == "GET":
+        return render_template("prideti.html")
+
+@app.route("/")
+def sarasas():
+        biudzetas = Irasas.query.all()
+        print(biudzetas)
+        return render_template("sarasas.html", biudzetas=biudzetas)
+
+if __name__ == "__main__":
+    app.run(debug=True)
